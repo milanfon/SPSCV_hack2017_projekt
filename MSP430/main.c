626e6944efd81da1ef98d9b64d1015186adf850a
Milan Horínek <milanfon@Milan-MacBook-Air.local>
2017-12-08 17:26:25 +0100

    Test

--- MSP430/main.c
+++ MSP430/main.c
@@ -1,9 +1,20 @@
+#include <msp430g2553.h>
+#include <stdint.h>
 
-
-/**
- * main.c
- */
 int main(void)
 {
-	return 0;
+    WDTCTL = WDTPW | WDTHOLD;       // Stop watchdog timer
+        P1DIR |= 0x01;                  // Set P1.0 to output direction
+
+        for(;;) {
+            volatile unsigned int i;    // volatile to prevent optimization
+
+            P1OUT ^= 0x01;              // Toggle P1.0 using exclusive-OR
+
+            i = 10000;                  // SW Delay
+            do i--;
+            while(i != 0);
+        }
+
+        return 0;
 }
