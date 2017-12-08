145ca0cf32f80cca5e8834d386c62385af4f0085
0Dangel <dan@sipek.org>
2017-12-08 21:06:17 +0100

    Wifina

--- Ovladani_voda/Ovladani_voda.ino
+++ Ovladani_voda/Ovladani_voda.ino
@@ -2,12 +2,32 @@
 
 char ssid[] = "Milanfon Mobile Network";
 char pass[] = "justforme";
+byte mac[6];
+
+#define LED RED_LED
 
 void setup() {
+  Serial.begin(9600);
   WiFi.begin(ssid, pass);
+  pinMode(LED, OUTPUT);
+
+  WiFi.macAddress(mac);
+
+  Serial.print("MAC: ");
+  Serial.print(mac[5],HEX);
+  Serial.print(":");
+  Serial.print(mac[4],HEX);
+  Serial.print(":");
+  Serial.print(mac[3],HEX);
+  Serial.print(":");
+  Serial.print(mac[2],HEX);
+  Serial.print(":");
+  Serial.print(mac[1],HEX);
+  Serial.print(":");
+  Serial.println(mac[0],HEX);
 }
 
 void loop() {
   // put your main code here, to run repeatedly: 
-  
+  digitalWrite(LED,HIGH);
 }
