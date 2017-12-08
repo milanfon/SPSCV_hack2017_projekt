8bf71ac8332dc3da77595730ecf9ef4ff05800c1
Milan Horínek <milanfon@Milan-MacBook-Air.local>
2017-12-08 15:21:01 +0100

    Texas

---
+++ Ovladani_voda/Ovladani_voda.ino
@@ -0,0 +1,13 @@
+#include <WiFi.h>
+
+char ssid[] = "Milanfon Mobile Network";
+char pass[] = "justforme";
+
+void setup() {
+  WiFi.begin(ssid, pass);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly: 
+  
+}
