b8d851eb6b5df38a05aaea83ba50b167e0e84f61
0Dangel <dan@sipek.org>
2017-12-08 19:05:55 +0100

    funkční

--- Arduino_hlavni_program/Arduino_hlavni_program.ino
+++ Arduino_hlavni_program/Arduino_hlavni_program.ino
@@ -1,4 +1,4 @@
-#include <dht.h>
+#include <DHT.h>
 
 int analogPin = 0;
 int raw= 0;
@@ -7,13 +7,15 @@ float Vout= 0;
 float R1= 1000;
 float R2= 0;
 float buffer= 0;
-dht DHT;
 
+#define typeDHT DHT22
 #define DHT11_PIN 2
+DHT dht(DHT11_PIN, typeDHT);
 
 void setup()
 {
   Serial.begin(9600);
+  dht.begin();
 }
 
 void loop()
@@ -37,10 +39,16 @@ void loop()
    }
   
   // Humidity
-  int chk = DHT.read11(DHT11_PIN);
+  float temp = dht.readTemperature();
+  float humid = dht.readHumidity();
+  if(isnan(temp) || isnan(humid)) {
+    Serial.print("chyba při čtení");
+  }
+    else{
   Serial.print("Temperature = ");
-  Serial.println(DHT.temperature);
+  Serial.println(temp);
   Serial.print("Humidity = ");
-  Serial.println(DHT.humidity);
+  Serial.println(humid);
+  }
   delay(1000);
 }
